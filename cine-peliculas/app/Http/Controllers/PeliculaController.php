fb22d98994e20b44b441816397ed9924ffcd4235
AlvaroMoCa <almaseb_123@hotmail.com>
2017-11-21 23:03:46 -0500

    mejorando el controlador con las rutas

--- cine-peliculas/app/Http/Controllers/PeliculaController.php
+++ cine-peliculas/app/Http/Controllers/PeliculaController.php
@@ -8,14 +8,19 @@ use App\Http\Requests;
 
 class PeliculaController extends Controller
 {
-    //
+    public function inicio(){
+    	return view('welcome');
+    }
+    public function vista(){
+    	return view('ingresar');
+    }
     public function create (Request $request){
     	$pelicula = new Pelicula();
     	$pelicula -> nombre = $request -> nombre;
     	$pelicula -> duracion = $request -> duracion;
     	$pelicula -> actor = $request -> actor;
     	$pelicula -> save();
-    	return redirect('/')
+    	return redirect('ingresar')
 
     }
 }
--- cine-peliculas/app/Http/routes.php
+++ cine-peliculas/app/Http/routes.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/create','PeliculaController@vista');
+Route::post('/create','PeliculaController@create');
+Route::get('/','PeliculaController@inicio');
